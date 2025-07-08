5683d149eb5058bc548e37a85c8953c1bfab0549
AngelDown12 <angeldown12xz@gmail.com>
2025-07-08 12:11:52 -0600

    Actualizar group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -7,7 +7,7 @@ const handler = async (m, { conn, text, participants, isAdmin, isBotAdmin, isOwn
   const command = m.text?.split(' ')[0] || '';
   const contenido = text?.replace(new RegExp(`^${command}\\s*`, 'i'), '').trim();
 
-  const firma = '> Cristian es gay aviso Cristian es gay aviso, Cristian es gay';
+  const firma = '> xd';
   const mensaje = contenido ? `${contenido}\n\n${firma}` : firma;
   const options = { mentions: users, quoted: m };
 
